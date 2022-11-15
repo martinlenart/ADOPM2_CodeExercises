9bf5795254b4c2622959cd4a154ac7888802a90f
Martin Lenart <martin_lenart@icloud.com>
2022-11-15 12:14:15 +0100

    Add project files.

---
+++ FriendList/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace FriendList // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
