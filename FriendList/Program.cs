8924a50741855e9907ed91e09f0e4d7e60cc2760
Martin Lenart <martin_lenart@icloud.com>
2022-11-15 15:46:28 +0100

    Step 1 complete

--- FriendList/Program.cs
+++ FriendList/Program.cs
@@ -10,6 +10,31 @@ namespace FriendList // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Friend f1 = new Friend {
+                FirstName = "Mikael", LastName = "Petersen",
+                Email = "m.petersen@gmail.com", Address= new AddressType {
+                    Street = "Mainstreet 1", Zip=12345, City="Gavle", Country="Sweden"}
+            };
+
+            Console.WriteLine(f1);
+
+            Friend f2 = new Friend("Anne", "Sterling", "a.sterling@icloud.com",
+                new AddressType { Street = "Backstreet 3", Zip = 98765, City = "Sigtuna", Country = "Sweden" });
+
+            Console.WriteLine(f2);
+
+            Friend f3 = Friend.Factory.CreateRandom();
+            Console.WriteLine(f3);
+
+            List<Friend> myFriends = new List<Friend>();
+            for (int i = 0; i < 10; i++)
+            {
+                myFriends.Add(Friend.Factory.CreateRandom());
+            }
+
+            Console.WriteLine(myFriends[0]);
+            Console.WriteLine(myFriends[myFriends.Count-1]);
         }
     }
 }
