b71dae92aaeb11662f727c0a7153817e9b4b4255
Martin Lenart <martin_lenart@icloud.com>
2022-11-21 15:55:29 +0100

    update

---
+++ Cars/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public enum CarMake { BMW, Honda, Jaguar, Volvo, VW }
+    internal class Car : IEquatable<Car>, IComparable<Car>
+    {
+        public CarMake Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+ 
+        public override string ToString()
+        {
+            return $"A {Make} {Model} of year {Year}";
+        }
+        
+        #region  IEquatable
+        public bool Equals(Car other)
+        {
+            if (Make == other.Make && Model == other.Model && Year == other.Year)
+                return true ;
+
+            return false ;
+           //return (Make, Model, Year) == (other.Make, other.Model, other.Year);
+        }
+
+        public override bool Equals(object obj) => Equals(obj as Car);
+        public override int GetHashCode() => (Make, Model, Year).GetHashCode();
+
+        public int CompareTo(Car other)
+        {
+            return Make.CompareTo(other.Make);
+            /*
+            if (this.Year < other.Year) return -1 ;
+            else if (this.Year> other.Year) return 1 ;
+            else
+            return 0;
+            */
+        }
+        #endregion
+
+        #region operator overload
+        public static bool operator ==(Car c1, Car c2) => c1.Equals(c2);
+        public static bool operator !=(Car c1, Car c2) => !c1.Equals(c2);
+        #endregion
+
+        public Car() { }
+    }
+}
---
+++ Cars/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Cars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car { Make = CarMake.BMW, Model = "3 series", Year = 2021 };
+            Car c2 = new Car { Make = CarMake.Volvo, Model = "V70", Year = 2020 };
+            Car c3 = new Car { Make = CarMake.Honda, Model = "Civic", Year = 2019 };
+            Car c4 = new Car { Make = CarMake.Volvo, Model = "V70", Year = 2020 };
+            Car c5 = new Car { Make = CarMake.BMW, Model = "5 series", Year = 2017 };
+
+
+            Console.WriteLine(c2);
+            Console.WriteLine(c4);
+
+
+            Console.WriteLine(c2 == c4);
+
+
+            List<Car> myCars = new List<Car>();
+            Car aCar = new Car { Make = CarMake.BMW, Model = "V70", Year = 2020 };
+            myCars.Add(c1);
+            myCars.Add(c2);
+            myCars.Add(c3);
+            myCars.Add(c4);
+            myCars.Add(c5);
+
+
+            var found = myCars.Contains(aCar);
+            Console.WriteLine(found);
+
+            foreach (var car in myCars)
+            {
+                if (car == aCar)
+                    Console.WriteLine("Car found");
+            }
+
+            myCars.Sort();
+            foreach (var car in myCars)
+            {
+                Console.WriteLine(car);
+            }
+
+
+        }
+    }
+}
