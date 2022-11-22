4101e09df9851ac18c1d9e6df882eeda0a8143f4
Martin Lenart <martin_lenart@icloud.com>
2022-11-22 12:44:13 +0100

    update

---
+++ Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        public class Chef
+        {
+            public string Name { get; set; } = "Boring";
+            public string Hello => $"Hi, I'm {Name}!";
+            public string FavoriteDish => "really nothing";
+            public override string ToString() => $"{Hello} My favorite dish is {FavoriteDish}.";
+        }
+
+        //Declare a French Chef, Pierre who loves Escargot
+
+        //Declare a German Chef, Klaus who loves Bratwurst
+
+        //Declare a SouthGerman Chef, Heinz who loves Sauerkraut
+
+        //Declare a Swedish Chef, Mats who loves Meatballs
+
+
+        static void Main(string[] args)
+        {
+            //Inheritance
+            Chef chef = new Chef();
+            Console.WriteLine(chef);
+
+            //Create the French, German, Swedish Chef
+
+
+            //Polymorfism
+            List<Chef> list = new List<Chef>();
+
+            //Add to the list the chef, French, German and Swedish chefs
+
+
+            //Printout all chefs in list so it shows the child
+
+        }
+    }
+}
