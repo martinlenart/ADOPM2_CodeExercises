e38de0993043d8655dbed5d721130e83be4a7068
Martin Lenart <martin_lenart@icloud.com>
2022-11-30 13:35:45 +0100

    update

--- RefValue/Bank.cs
+++ RefValue/Bank.cs
@@ -11,14 +11,14 @@ namespace RefValue
         public string Name { get; set; } = "SEB";
         public void Deposit(csAccount acc, decimal Amount) {
             acc.Saldo = 1000;
-            acc.sLastTransaction = $"Added {1000:C2}";
-            acc.sbLastTransaction.Append($"Added {1000:C2}");
+            acc.sLastTransaction = $"Added {Amount:C2}";
+            acc.sbLastTransaction.Append($"Added {Amount:C2}");
         }
         public void Deposit(stAccount acc, decimal Amount)
         {
             acc.Saldo = 1000;
-            acc.sLastTransaction = $"Added {1000:C2}";
-            acc.sbLastTransaction.Append($"Added {1000:C2}");
+            acc.sLastTransaction = $"Added {Amount:C2}";
+            acc.sbLastTransaction.Append($"Added {Amount:C2}");
         }
     }
 }
